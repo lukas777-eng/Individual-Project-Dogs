263e87965e90acd1da25912d2b8229903a04d382
lukas777-eng <lucastomascasco@gmail.com>
2022-02-13 03:33:18 -0300

    add database

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,54 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+const axios = require('axios');
+const { Dog, Temperament } = require('../db');
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+
+///trayendo datos de la api
+const getApiInfo = async () => {
+    const apiUrl = await axios.get('https://api.thedogapi.com/v1/breeds');
+    const apiInfo = await apiUrl.data.map(el => {
+        return {
+            id: el.id,
+            name: el.name,
+            height: el.height,
+            weight: el.weight,
+        };
+    });
+    return apiInfo;
+
+};
+
+//// trayendo bases de datos
+const getDbInfo = async () => {
+    return await Dog.findAll({
+        include:{
+            model: Temperament,
+            attributes: ['name'],
+            through: {
+                atrributes: [],
+            },
+        }
+    })
+}
+
+////concatenando los datos de la api y DB
+const getAllDogs = async () => {
+    const apiInfo = await getApiInfo();
+    const dbInfo = await getDbInfo();
+    const allInfo = apiInfo.concat(dbInfo);
+    return allInfo;
+}
+
+router.get('/dogs', async (req, res) => {
+    let totalDogs = await getAllDogs();
+    res.status(200).send(totalDogs)
+})
 
 
 module.exports = router;
