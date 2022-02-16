6bc97c4705e6c0b25bbc6e648cb1479d27879467
lukas777-eng <lucastomascasco@gmail.com>
2022-02-16 00:27:53 -0300

    update

---
+++ client/src/components/dogHome.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getDogs } from '../actions';
+import { Link } from 'react-router-dom';
+import dogCard from '../components/dogCard';
+
+export default function dogHome(){
+    const dispatch = useDispatch()
+    const allDogs = useSelector ((state) => state.dogs)
+
+    useEffect(() => {
+        dispatch(getDogs())
+    },[]);
+
+    function handleClick(e){
+        e.preventDefault();
+        dispatch(getDogs());
+    }
+    return (
+        <div>
+            <Link to="/dogs">create doggy</Link>
+            <h1> doggy</h1>
+            <button onClick={e => {handleClick(e)}}> reload dogs</button>
+            <div>
+                <select>
+                    <option value='selected'>sortByName</option>
+                    <option value='asc'>A-Z</option>
+                    <option value='desc'>Z-A</option>
+                </select>
+                <select>
+                    <option value='selected'>SortByWeight</option>
+                    <option value='asc'>Lighter to heavier</option>
+                    <option value='desc'>Heavier to lighter</option>
+                </select>
+                <select>
+                    <option value='all'>All breeds</option>
+                    <option value='api'>Existent breeds</option>
+                    <option value='created'>Created breeds</option>
+                </select>
+                {
+                    allDogs && allDogs.map( el => {
+                        <dogCard image={el.img} name={el.name} temperament={el.temperament} weight={el.weight} />
+                    })
+                }
+            </div>
+        </div>
+    )
+}
--- client/src/reducer/index.js
+++ client/src/reducer/index.js
@@ -1,4 +1,17 @@
-function rootReducer(){
+const initialState = {
+    dogs : []
+}
+
+
+
+function rootReducer(state = initialState, action){
+    switch(action.type){
+        case 'GET_DOGS':
+            return{
+                ...state,
+                dogs: action.payload
+            }   
+    }
 
 }
 
