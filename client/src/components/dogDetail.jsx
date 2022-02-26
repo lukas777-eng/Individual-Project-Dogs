7b5056c304d3cf4412ede0819c8b4724be467e67
lukas777-eng <lucastomascasco@gmail.com>
2022-02-26 13:25:39 -0300

    add detail css

--- client/src/components/dogDetail.jsx
+++ client/src/components/dogDetail.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { getDetail } from '../actions/index.js';
 import { useEffect } from 'react';
+import './dogDetail.css';
 
 export default function Detail(props){
     console.log(props)
@@ -15,20 +16,26 @@ export default function Detail(props){
     const myDog = useSelector((state) => state.detail)
 
     return (
-        <div>
+        <div >
             {
                 myDog.length > 0 ?
-                <div>
+                <div className='detail'>
+                    <div>
+                    <img className="imgDetail"src= {myDog[0].img? myDog[0].img : myDog[0].image} alt="not found  "/>
+                    </div>
+                    <div className='detailInfo'>
                     <h1>{myDog[0].name}</h1>
-                    <img src= {myDog[0].img? myDog[0].img : myDog[0].image} alt="not found  "/>
-                    <h2>{myDog[0].weight}</h2>
-                    <h2>{myDog[0].temperament}</h2>
-                    <h2>{myDog[0].life_span}</h2>
+                    <h2>Weight:{myDog[0].weight}</h2>
+                    <h2>Life span:{myDog[0].life_span}</h2>
+                    <h2>Temperament:{myDog[0].temperament}</h2>
+
+                    <Link to= '/DogHome'>
+                <button className='back'>Back</button>
+            </Link>
+                    </div>
                 </div> : <p>Loading..</p>
             }
-            <Link to= '/DogHome'>
-                <button>Back</button>
-            </Link>
+            
         </div>
     )
 }
